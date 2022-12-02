a56137883b4fd67e43d24978422c583a68deb825
Ibukunoluwa Oyeniyi <ibukun46@gmail.com>
2022-12-02 19:32:51 +0100

    updated the bot to calculate amount and list

--- testing.py
+++ testing.py
@@ -1,5 +1,7 @@
 import os
 import telegram.ext
+from telegram.ext.messagehandler import MessageHandler
+from telegram.ext.filters import Filters
 import requests
 from app.utils import get_binancep2p_rate, format_binance_response_data
 import asyncio
@@ -26,7 +28,18 @@ def help(update,context):
 /usd -> You type in the isocode of the currency and it returns the black market rate of the currency
 /convert -> You type in the isocode of the currency you want to convert then the isocode of the currency you want to convert to.
 e.g /convert USD NGN
-""")
+/list -> Lists all the avalable iso_cades the bot can get rates for
+/calculate -> Calculate the actual amount of any available currency to another put the from and to currency.
+e.g /calculate USD NGN 
+then you type in the amount you want
+"""
+    )
+
+def list(update,context):
+    for iso_code in iso_code_list:
+        update.message.reply_text(iso_code)
+        
+
 
 async def usd(update,context):
     value = "".join(context.args[0]).upper()
@@ -54,6 +67,24 @@ async def convert(update,context):
         final_result = round(float(parallel_buy2) / float(parallel_buy1),2)
         reply = f"1 {from_currency} to {to_currency} is {final_result}"
         update.message.reply_text(reply)
+        
+async def calculate(update,context):
+    from_currency = "".join(context.args[0]).upper()
+    to_currency = "".join(context.args[1]).upper()
+    if from_currency not in iso_code_list and to_currency not in iso_code_list:
+        update.message.reply_text("One of this isocodes does not have a black market rate")
+    else:
+        update.message.reply_text(f"Enter the amount of {from_currency}")
+        value = update.message.text
+        resp_data1 = await get_binancep2p_rate(from_currency)
+        formated_data1 = await format_binance_response_data(resp_data1)
+        resp_data2 = await get_binancep2p_rate(to_currency)
+        formated_data2 = await format_binance_response_data(resp_data2)
+        parallel_buy1 = formated_data1["buy_rate"]
+        parallel_buy2 = formated_data2["buy_rate"]
+        final_result = round(float(parallel_buy2) / float(parallel_buy1),2) * value
+        reply = f"{value} {from_currency} to {to_currency} is {final_result}"
+        update.message.reply_text(reply)
 
 def loop_runner(action):
     loop = asyncio.new_event_loop()
@@ -63,17 +94,26 @@ def loop_runner_2(action):
     loop = asyncio.new_event_loop()
     return loop.run_until_complete(action)
 
+def loop_runner_3(action):
+    loop = asyncio.new_event_loop()
+    return loop.run_until_complete(action)
+
 def sync_run(update,context):
     return loop_runner(usd(update,context))
 
 def sync_run_2(update,context):
     return loop_runner_2(convert(update,context))
 
+def sync_run_3(update,context):
+    return loop_runner_3(calculate(update,context))
+
 
 dispatcher.add_handler(telegram.ext.CommandHandler('start',start))
 dispatcher.add_handler(telegram.ext.CommandHandler('help',help))
+dispatcher.add_handler(telegram.ext.CommandHandler('list',list))
 dispatcher.add_handler(telegram.ext.CommandHandler('usd',sync_run))
 dispatcher.add_handler(telegram.ext.CommandHandler('convert',sync_run_2))
+dispatcher.add_handler(telegram.ext.CommandHandler('calculate',sync_run_3))
 
 if __name__ == "__main__":
     updater.start_polling()
